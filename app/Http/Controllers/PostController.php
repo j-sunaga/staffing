5ee0bcc947aae13ead25a3e4f8e65da1999ce0d6
j-sunaga <124577blue@gmail.com>
2020-07-11 09:48:36 +0900

    postコントローラにindexアクションを追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -13,7 +14,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+
+        return view('posts.index', compact('posts'));
     }
 
     /**

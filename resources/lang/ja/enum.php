dc4b85e4c5accdb5f2ea9b03ad256b8c6e6fc0e5
j-sunaga <124577blue@gmail.com>
2020-07-14 07:12:04 +0900

    enumの値に対応する日本語の値を追加

---
+++ resources/lang/ja/enum.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+  'check_status_0' => '未着手',
+  'check_status_1' => '着手中',
+  'check_status_2' => '完了',
+  'check_priority_0' => '低',
+  'check_priority_1' => '中',
+  'check_priority_2' => '高',
+];

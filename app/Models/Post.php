95a869402bc32eac4b031473e0b7f1fe3866d636
j-sunaga <124577blue@gmail.com>
2020-07-13 20:01:02 +0900

    モデル列をenumにキャストするためのメソッドを追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,25 @@
 
 namespace App\Models;
 
+use BenSampo\Enum\Traits\CastsEnums;
+use App\Enums\CheckStatus;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+
     public function user()
     {
         return $this->belongsTo('App\User');
     }
+
+    use CastsEnums;
+
+    protected $enumCasts = [
+        'status' => CheckStatus::class, // Example enum cast
+    ];
+
+    protected $casts = [
+        'status' => 'int',
+    ];
 }

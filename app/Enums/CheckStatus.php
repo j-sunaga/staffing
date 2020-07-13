d02ad96be1755942f6ff7467f27c4a8345ad9be7
j-sunaga <124577blue@gmail.com>
2020-07-14 07:10:21 +0900

    descriptionメソッドの追加

--- app/Enums/CheckStatus.php
+++ app/Enums/CheckStatus.php
@@ -3,6 +3,7 @@
 namespace App\Enums;
 
 use BenSampo\Enum\Enum;
+use Lang;
 
 /**
  * @method static static OptionOne()
@@ -15,11 +16,11 @@ final class CheckStatus extends Enum
     const Enabled = 1;
     const Completed = 2;
 
-    // public static function getDescription($value): string
-    // {
-    //     if (Lang::has('enum.check_post_data.' . $value)) {
-    //         return Lang::get('enum.check_post_data.' . $value);
-    //     }
-    //     return parent::getDescription($value);
-    // }
+    public static function getDescription($value): string
+    {
+        if (Lang::has('enum.check_status_' . $value)) {
+            return Lang::get('enum.check_status_' . $value);
+        }
+        return parent::getDescription($value);
+    }
 }

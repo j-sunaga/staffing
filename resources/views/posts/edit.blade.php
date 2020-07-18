7a1c302ba249332eedbf1aa7f0a95ab8ff344ae6
j-sunaga <124577blue@gmail.com>
2020-07-18 10:13:39 +0900

    edit画面にカテゴリを表示

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -52,6 +52,24 @@
       </div>
     </div>
 
+    <div class="row">
+      <div class="form-group col-md-6">
+        <label>カテゴリ</label>
+        <div>
+          @foreach ($post->categories as $category)
+          <div class="form-check">
+            @if ($post->categories->contains('id', $category->id))
+            <input name="category[]" type="checkbox" value="{{ $category->id }}" checked>
+            @else
+            <input name="category[]" type="checkbox" value="{{ $category->id }}">
+            @endif
+            <label class='badge badge-secondary'>{{ $category->category_name }}</label>
+          </div>
+          @endforeach
+        </div>
+      </div>
+    </div>
+
     <div class="form-group col-md-6">
       <button type="submit" class="btn btn-primary" name="commit">更新</button>
     </div>
